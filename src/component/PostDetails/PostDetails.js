438fcb67b24e4dbe1322155152205962cafcf1b8
Siddika-akhter <siddika.tas28@gmail.com>
2020-09-25 00:42:36 +0600

    card push

--- src/component/PostDetails/PostDetails.js
+++ src/component/PostDetails/PostDetails.js
@@ -1,26 +1,45 @@
-import React, { useEffect, useState } from 'react';
-import { Card } from 'react-bootstrap';
+import React from 'react';
 import { useParams } from 'react-router-dom';
 import CalenderDate from '../CalenderDate/CalenderDate';
 import { travelPlaces } from '../Home/Home';
 import './PostDetails.css'
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+
+const useStyles = makeStyles({
+    root: {
+        maxWidth: 345,
+    },
+});
+
 
 
 const PostDetails = () => {
+    const classes = useStyles();
     const { placeKey } = useParams();
+    const post = travelPlaces.find(a => a.key === placeKey)
 
-    const post = travelPlaces.find(place => console.log(place));
-    // console.log(post)
-    console.log(useParams());
     return (
 
         <div className="container d-flex">
-            <div className="col-md-6">
-                <h3>{placeKey} comming soon</h3>
-            </div>
+            <Card className={classes.root}>
+                <CardActionArea>
+                    <CardContent>
+                        <Typography gutterBottom variant="h3" component="h2">
+                            {post.destination}
+                        </Typography>
+                        <Typography variant="body2" color="textSecondary" component="p">
+                           {post.description}
+                        </Typography>
+                    </CardContent>
+                </CardActionArea>
 
+            </Card>
 
-            <div className="calender col-md-6">
+            <div className="calender col-md-6 ml-5">
                 <CalenderDate></CalenderDate>
             </div>
         </div>
