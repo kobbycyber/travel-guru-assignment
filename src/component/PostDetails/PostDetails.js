b3918424a132c9060d6d73b6e8438043740e45bc
Siddika-akhter <siddika.tas28@gmail.com>
2020-09-24 23:37:05 +0600

    calender component push

--- src/component/PostDetails/PostDetails.js
+++ src/component/PostDetails/PostDetails.js
@@ -8,15 +8,21 @@ import './PostDetails.css'
 
 const PostDetails = () => {
     const { placeKey } = useParams();
-    
-    const post = travelPlaces.find(place => console.log(place) );
-        // console.log(post)
-        console.log(useParams());
+
+    const post = travelPlaces.find(place => console.log(place));
+    // console.log(post)
+    console.log(useParams());
     return (
 
-        <div className="container">
-            <h3>{placeKey} comming soon</h3>
-          
+        <div className="container d-flex">
+            <div className="col-md-6">
+                <h3>{placeKey} comming soon</h3>
+            </div>
+
+
+            <div className="calender col-md-6">
+                <CalenderDate></CalenderDate>
+            </div>
         </div>
 
     );
