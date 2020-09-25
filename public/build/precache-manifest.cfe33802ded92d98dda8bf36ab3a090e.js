e8553c22a25f0e5560d2b5db985623a0d1dbdc50
Siddika-akhter <siddika.tas28@gmail.com>
2020-09-25 21:59:53 +0600

    file add

---
+++ public/build/precache-manifest.cfe33802ded92d98dda8bf36ab3a090e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1eebc36acff7fa2f672890b11ba7a3aa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "71aa4e8889fca55cebb0",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "af66aa0b1c1e0d135895",
+    "url": "/static/css/main.be053bc8.chunk.css"
+  },
+  {
+    "revision": "71aa4e8889fca55cebb0",
+    "url": "/static/js/2.52fe3e7a.chunk.js"
+  },
+  {
+    "revision": "7024ffe691007b10356d66fde7a25e6f",
+    "url": "/static/js/2.52fe3e7a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af66aa0b1c1e0d135895",
+    "url": "/static/js/main.fea522d7.chunk.js"
+  },
+  {
+    "revision": "db37560cb65ddb8a460c",
+    "url": "/static/js/runtime-main.92e2487c.js"
+  },
+  {
+    "revision": "489a4840241959c79f00a9f12dbb7d22",
+    "url": "/static/media/Logo.489a4840.png"
+  },
+  {
+    "revision": "32162f64d326b339d4a75b6d64d9e6c7",
+    "url": "/static/media/background.32162f64.png"
+  }
+]);
